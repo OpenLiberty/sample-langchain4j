e2613495d931fc8c011a3f37a8651cbe860b2615
George Zhang <geetransit@gmail.com>
2025-06-25 16:09:46 -0400

    Initial streaming chat commit

---
+++ streaming-chat/src/main/java/dev/langchain4j/example/streaming/chat/StreamingChatAgent.java
@@ -0,0 +1,68 @@
+package dev.langchain4j.example.streaming.chat;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import dev.langchain4j.example.util.ModelBuilder;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.output.FinishReason;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.TokenStream;
+import dev.langchain4j.service.UserMessage;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class StreamingChatAgent {
+
+    @Inject
+    private ModelBuilder modelBuilder;
+
+    @Inject
+    @ConfigProperty(name = "chat.memory.max.messages")
+    private Integer MAX_MESSAGES;
+
+    @FunctionalInterface
+    interface PartialResponseHandler {
+        void onPartialResponse(String token) throws Exception;
+    }
+
+    interface StreamingAssistant {
+       TokenStream streamingChat(@MemoryId String sessionId, @UserMessage String userMessage);
+    }
+
+    private StreamingAssistant assistant = null;
+
+    public StreamingAssistant getStreamingAssistant() throws Exception {
+        if (assistant == null) {
+            StreamingChatModel streamingModel = modelBuilder.getStreamingChatModel();
+            assistant = AiServices.builder(StreamingAssistant.class)
+                .streamingChatModel(streamingModel)
+                .chatMemoryProvider(
+                    sessionId -> MessageWindowChatMemory.withMaxMessages(MAX_MESSAGES))
+                .build();
+        }
+        return assistant;
+    }
+
+    public FinishReason streamingChat(String sessionId, String message, PartialResponseHandler handler) throws Exception {
+        CompletableFuture<ChatResponse> future = new CompletableFuture<>();
+        getStreamingAssistant().streamingChat(sessionId, message)
+            .onPartialResponse(token -> {
+                try {
+                    handler.onPartialResponse(token);
+                } catch (Throwable t) {
+                    future.completeExceptionally(t);
+                }
+            })
+            .onCompleteResponse(future::complete)
+            .onError(future::completeExceptionally)
+            .start();
+        return future.get().finishReason();
+    }
+
+}
