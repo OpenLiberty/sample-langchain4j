c7f8edfe5f24b29028c9ae3f3062cbdc2e75928e
George Zhang <geetransit@gmail.com>
2025-07-04 17:40:43 -0400

    Initial streaming chat commit (#5)
    * Initial streaming chat commit
    
    * Remove unnecessary dependencies and unused code
    
    * Fix whitespace issues
    
    * Use io.openliberty.sample.langchain4j package
    
    * Fix newlines in response not shown on website
    
    * Update langchain4j to 1.1.0

---
+++ streaming-chat/src/main/java/io/openliberty/sample/langchain4j/streaming/chat/StreamingChatService.java
@@ -0,0 +1,74 @@
+package io.openliberty.sample.langchain4j.streaming.chat;
+
+import java.util.logging.Logger;
+
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.websocket.CloseReason;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.RemoteEndpoint;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ApplicationScoped
+@ServerEndpoint("/streamingchat")
+public class StreamingChatService {
+
+    private static Logger logger = Logger.getLogger(StreamingChatService.class.getName());
+
+    @Inject
+    StreamingChatAgent agent = null;
+
+    @OnOpen
+    public void onOpen(Session session) {
+        logger.info("Server connected to session: " + session.getId());
+    }
+
+    @OnMessage
+    @Timed(name = "chatProcessingTime", absolute = true,
+           description = "Time needed chatting to the agent.")
+    public void onMessage(String message, Session session) throws Exception {
+
+        logger.info("Server received message \"" + message + "\" "
+                    + "from session: " + session.getId());
+
+        RemoteEndpoint.Basic remote = session.getBasicRemote();
+
+        try {
+            String sessionId = session.getId();
+            switch (agent.streamingChat(sessionId, message, token -> {
+                remote.sendText(token);
+                Thread.sleep(100);
+            })) {
+            case STOP:
+                break;
+            default:
+                remote.sendText(" ...");
+            }
+        } catch (Exception e) {
+            remote.sendText("My failure reason is:\n\n" + e.getMessage());
+        }
+
+        remote.sendText("");
+        logger.info("Server finished response to session: " + session.getId());
+
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        logger.info("Session " + session.getId()
+                    + " was closed with reason " + closeReason.getCloseCode());
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        logger.info("WebSocket error for " + session.getId() + " "
+                    + throwable.getMessage());
+    }
+
+}
