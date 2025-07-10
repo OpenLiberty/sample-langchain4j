4f1db621f57c99855f440eac0b6cce29c80bbf1c
Siaa Gor <Siaa.Gor@ibm.com>
2025-07-10 15:13:41 -0400

    added TLS/SSL encryption when connecting to mongodb atlas
    Signed-off-by: Siaa Gor <Siaa.Gor@ibm.com>

--- rag/src/main/java/dev/langchain4j/example/chat/MongodbClient.java
+++ rag/src/main/java/dev/langchain4j/example/chat/MongodbClient.java
@@ -9,12 +9,16 @@
  *******************************************************************************/
 package dev.langchain4j.example.chat;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class MongodbClient {
@@ -22,13 +26,25 @@ public class MongodbClient {
     @ConfigProperty(name = "mongo.uri.connection", defaultValue = "")
     private String MOGODB_CONNECTION_STRING;
 
+    private static Logger logger = Logger.getLogger(MongodbClient.class.getName());
+
     @Produces
     public MongoClient createMongodbClient() {
+
         if (MOGODB_CONNECTION_STRING == "" || MOGODB_CONNECTION_STRING == null
                 || MOGODB_CONNECTION_STRING.equals("set it by env variable")) {
             throw new IllegalStateException("Mongodb Connection string can not be empty");
         }
-        return MongoClients.create(MOGODB_CONNECTION_STRING);
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(MOGODB_CONNECTION_STRING))
+                .applyToSslSettings(builder -> builder.enabled(true))
+                .build();
+
+        MongoClient mongoClient = MongoClients.create(settings);
+        logger.info("Enabled TLS/SSL when connecting to MongoDB Atlas");
+
+        return mongoClient;
 
     }
 }
