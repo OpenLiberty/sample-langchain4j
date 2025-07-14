10d11a8c5857655ea524e4d407ad0b676d52fb2c
Gilbert Kwan <gkwan@ca.ibm.com>
2025-07-14 16:24:13 -0400

    rag sample core
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

---
+++ rag/src/test/java/it/io/openliberty/sample/langchan4j/RAGChatServiceIT.java
@@ -0,0 +1,75 @@
+package it.io.openliberty.sample.langchan4j;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class RAGChatServiceIT {
+
+    private static CountDownLatch countDown;
+
+    private static RAGChatClient client;
+    
+    @BeforeEach
+    public void setup() throws Exception {
+        countDown = new CountDownLatch(1);
+        URI uri = new URI("ws://localhost:9080/chat");
+        client = new RAGChatClient(uri);
+    }
+
+    @AfterEach
+    public void teardown() throws Exception {
+      client.close();
+    }
+
+    @Test
+    public void testJakartaEE() throws Exception {
+        if (Util.usingHuggingFace()) {
+            return;
+        }
+
+        client.sendMessage("What are some current problems users have with JakartaEE?");
+        countDown.await(120, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testMicroProfile() throws Exception {
+        if (Util.usingHuggingFace()) {
+            return;
+        }
+
+        client.sendMessage("What are some current problems users have with MicroProfile?");
+        countDown.await(120, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testLangChain4j() throws Exception {
+        if (Util.usingHuggingFace()) {
+            return;
+        }
+
+        client.sendMessage("What are some current problems users have with LangChain4J?");
+        countDown.await(120, TimeUnit.SECONDS);
+    }
+
+    public static void verify(String message) {
+        assertNotNull(message);
+
+        String text = message.toLowerCase();
+        assertTrue(text.contains("microprofile") ||
+            text.contains("jakarta") || text.contains("langchain"),
+            message);
+        countDown.countDown();
+    }
+
+}
