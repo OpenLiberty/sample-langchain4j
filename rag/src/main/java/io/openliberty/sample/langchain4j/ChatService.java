bb9e6d19bdad25db78b24cc9c119fbf5727954c3
Siaa Gor <Siaa.Gor@ibm.com>
2025-08-06 15:59:36 -0400

    implemented and tested Loading the embeddings from the text segments to the Mongodb Database using the Api calls defined in EmbeddingService.java. Also added an api in EmbeddingService.java to retrieve all embeddings. 1. user visits localhost:8080 the chat service -> All the files under knowledge_base are added to Mongodb 2. OpenApi ui, users are able to call the get method to see all embeddings from the database.
    Signed-off-by: Siaa Gor <Siaa.Gor@ibm.com>

--- rag/src/main/java/io/openliberty/sample/langchain4j/ChatService.java
+++ rag/src/main/java/io/openliberty/sample/langchain4j/ChatService.java
@@ -9,9 +9,21 @@
  *******************************************************************************/
 package io.openliberty.sample.langchain4j;
 
+import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.segment.TextSegment;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -22,21 +34,65 @@ import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
+import java.util.Base64;
 
 @ApplicationScoped
 @ServerEndpoint(value = "/chat", encoders = { ChatMessageEncoder.class })
 public class ChatService {
-
+   
     private static Logger logger = Logger.getLogger(ChatService.class.getName());
-
+    private final String ADMIN_USERNAME = "bob";
+    private final String ADMIN_PASSWD = "bobpwd";
     @Inject
     ChatAgent agent = null;
+    
 
     @OnOpen
     public void onOpen(Session session) {
         logger.info("Server connected to session: " + session.getId());
-    }
+        File file = new File("resources/knowledge_base");
+        String fullPath = file.getAbsolutePath();
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(fullPath, new ApacheTikaDocumentParser());
 
+        var docSplitter = DocumentSplitters.recursive(400, 50);
+
+        List<TextSegment> textSeg = docSplitter.splitAll(documents);
+        insertToDatabase(textSeg);
+    }
+    
+    public void insertToDatabase(List<TextSegment> contentSeg){
+        
+        String startURL = "http://localhost:9080/api/embedding?";
+        int i = 0;
+        for (TextSegment content : contentSeg){
+            i +=1;
+            String contentEncoded = "";
+            try{
+                contentEncoded = URLEncoder.encode(content.text(), "UTF-8");
+                String parameters = String.format("summary=%s&content=%s",i,contentEncoded);
+                try{
+                    URL url = new URI(startURL+parameters).toURL();
+                    HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+                    String login = ADMIN_USERNAME + ":" + ADMIN_PASSWD;
+                    String encodedCredentials = Base64.getEncoder().encodeToString((login).getBytes());
+                    String value = "Basic " + encodedCredentials;
+                    httpConnection.setRequestProperty("Authorization", value);
+                    httpConnection.setRequestMethod("POST");
+                    httpConnection.setRequestProperty("Accept", "*/*");
+                    httpConnection.setDoOutput(false);
+                    if (httpConnection.getResponseCode() != 200){
+                        logger.warning("Could not connect to api");
+                    }
+                    
+                }catch(Exception e){
+                    logger.warning("Could not load knowledge base into MongoDB.");
+                }
+            }catch(Exception e){
+                    logger.warning("Could not load knowledge base into MongoDB.");            
+            }
+        }
+
+    } 
     @OnMessage
     @Timed(name = "chatProcessingTime", absolute = true,
            description = "Time needed chatting to the agent.")
@@ -50,6 +106,9 @@ public class ChatService {
             String sessionId = session.getId();
             answer = agent.chat(sessionId, message);
         } catch (Exception e) {
+            //Work in progress area
+            // but running the full app works.
+            
             answer = "My failure reason is:\n\n" + e.getMessage();
         }
 
