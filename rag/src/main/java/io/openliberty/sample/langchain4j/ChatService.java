9b76ebbf173ff3153f6c3e25c6db4d36a0c839cd
Siaa Gor <Siaa.Gor@ibm.com>
2025-08-07 13:49:57 -0400

    Implemented and tested comparing embeddings/ getting top 3 content and coding the custom retrival augmentator - example query on localhost:9080 : Create Java class that uses the MicroProfile Health API to check/montior CPU usage is below 95%: and comparing to mp-health-3.txt. Also increase character length size for segments to keep simple
    Signed-off-by: Siaa Gor <Siaa.Gor@ibm.com>

--- rag/src/main/java/io/openliberty/sample/langchain4j/ChatService.java
+++ rag/src/main/java/io/openliberty/sample/langchain4j/ChatService.java
@@ -9,22 +9,33 @@
  *******************************************************************************/
 package io.openliberty.sample.langchain4j;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import org.bson.conversions.Bson;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
 
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.data.segment.TextSegment;
-
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.websocket.CloseReason;
@@ -34,19 +45,40 @@ import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Base64;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import org.apache.lucene.util.VectorUtil;
+
 
 @ApplicationScoped
 @ServerEndpoint(value = "/chat", encoders = { ChatMessageEncoder.class })
 public class ChatService {
    
     private static Logger logger = Logger.getLogger(ChatService.class.getName());
+
     private final String ADMIN_USERNAME = "bob";
+
     private final String ADMIN_PASSWD = "bobpwd";
+
+    private int MAX_RESULTS = 3;
+
+    private EmbeddingModel embModel = new AllMiniLmL6V2EmbeddingModel();
+    
+    private PriorityQueue<Map.Entry<String, Float>> maxHeap = new PriorityQueue<>(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
     @Inject
     ChatAgent agent = null;
     
+    @Inject
+    private MongoDatabase db;
 
+    
     @OnOpen
     public void onOpen(Session session) {
         logger.info("Server connected to session: " + session.getId());
@@ -54,7 +86,7 @@ public class ChatService {
         String fullPath = file.getAbsolutePath();
         List<Document> documents = FileSystemDocumentLoader.loadDocuments(fullPath, new ApacheTikaDocumentParser());
 
-        var docSplitter = DocumentSplitters.recursive(400, 50);
+        var docSplitter = DocumentSplitters.recursive(2500, 50);
 
         List<TextSegment> textSeg = docSplitter.splitAll(documents);
         insertToDatabase(textSeg);
@@ -64,6 +96,7 @@ public class ChatService {
         
         String startURL = "http://localhost:9080/api/embedding?";
         int i = 0;
+
         for (TextSegment content : contentSeg){
             i +=1;
             String contentEncoded = "";
@@ -104,11 +137,14 @@ public class ChatService {
         String answer;
         try {
             String sessionId = session.getId();
+            List<String> topThree = getSimilarContent(message);
+            message += "\nHere are the top similar content from the knowledge base (use relevent info only): ";;
+            for (String part : topThree){
+                message += "\n" + part;
+            }
             answer = agent.chat(sessionId, message);
+
         } catch (Exception e) {
-            //Work in progress area
-            // but running the full app works.
-            
             answer = "My failure reason is:\n\n" + e.getMessage();
         }
 
@@ -118,6 +154,55 @@ public class ChatService {
             e.printStackTrace();
         }
 
+    }
+    private byte[] toBytes(float[] vector) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try (DataOutputStream dos = new DataOutputStream(bos)) {
+            for (float f : vector) {
+                dos.writeFloat(f);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return bos.toByteArray();
+    }
+
+    public List<String> getSimilarContent(String userMessage){
+        byte[] messageVec = toBytes(embModel.embed(userMessage).content().vector());
+
+        List<String> result = new ArrayList<>();
+
+        MongoCollection<org.bson.Document> embeddingStore = db.getCollection("EmbeddingsStored");
+
+        Bson projection = Projections.fields(Projections.include( "Vector", "Content"));
+
+        FindIterable<org.bson.Document> docs = embeddingStore.find().projection(projection);
+
+        for (org.bson.Document d : docs) {
+
+            org.bson.types.Binary binaryVectorFormat = d.get("Vector", org.bson.types.Binary.class);
+            byte[] vec = binaryVectorFormat.getData();
+            Float similarity = VectorUtil.cosine(messageVec, vec);
+            maxHeap.offer(new AbstractMap.SimpleEntry<>((String) d.getString("Content"), similarity));
+            
+        }
+        
+        int currRes = 0;
+        while(!maxHeap.isEmpty()){
+            Map.Entry<String,Float> topThree = maxHeap.poll();
+
+            if (currRes <= MAX_RESULTS){
+
+                result.add(topThree.getKey());
+                currRes +=1;
+                
+            }else{
+                break;
+            }
+
+        }
+
+        return result;
     }
 
     @OnClose
