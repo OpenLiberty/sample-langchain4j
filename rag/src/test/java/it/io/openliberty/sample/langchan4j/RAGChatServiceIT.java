07859776dae27465dfbd055bcb4aed3092ac5398
Siaa Gor <Siaa.Gor@ibm.com>
2025-08-12 15:34:22 -0400

    for the above changes, modified RagChatServiceIT.java to initialize the database before asking questions: the database will only be initialized if it hasn't been already
    Signed-off-by: Siaa Gor <Siaa.Gor@ibm.com>

--- rag/src/test/java/it/io/openliberty/sample/langchan4j/RAGChatServiceIT.java
+++ rag/src/test/java/it/io/openliberty/sample/langchan4j/RAGChatServiceIT.java
@@ -3,13 +3,17 @@ package it.io.openliberty.sample.langchan4j;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
+import java.util.Base64;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -20,11 +24,39 @@ public class RAGChatServiceIT {
 
     private static RAGChatClient client;
     
+    private final String ADMIN_USERNAME = "bob";
+
+    private final String ADMIN_PASSWD = "bobpwd";
+
     @BeforeEach
     public void setup() throws Exception {
         countDown = new CountDownLatch(1);
         URI uri = new URI("ws://localhost:9080/chat");
         client = new RAGChatClient(uri);
+        initializeDatabase();
+    }
+    
+    public void initializeDatabase() throws Exception{
+        
+        String startURL = "http://localhost:9080/api/embedding/init";
+
+        try{
+
+            URL url = new URI(startURL).toURL();
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            String login = ADMIN_USERNAME + ":" + ADMIN_PASSWD;
+            String encodedCredentials = Base64.getEncoder().encodeToString((login).getBytes());
+            String value = "Basic " + encodedCredentials;
+            httpConnection.setRequestProperty("Authorization", value);
+            httpConnection.setRequestMethod("POST");
+            httpConnection.setRequestProperty("Accept", "*/*");
+            httpConnection.setDoOutput(false);
+
+        }catch(Exception exception){
+
+            throw new Exception("Error in initializing the knowledge base.");
+        
+        }
     }
 
     @AfterEach
