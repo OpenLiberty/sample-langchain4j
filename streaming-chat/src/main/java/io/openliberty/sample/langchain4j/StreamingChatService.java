d686b9529355b32a6df13eff7899246098082458
George Zhang <geetransit@gmail.com>
2025-08-28 17:01:41 -0400

    Simplify example a bit more

--- streaming-chat/src/main/java/io/openliberty/sample/langchain4j/StreamingChatService.java
+++ streaming-chat/src/main/java/io/openliberty/sample/langchain4j/StreamingChatService.java
@@ -10,10 +10,11 @@
 
 package io.openliberty.sample.langchain4j;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
-import org.eclipse.microprofile.metrics.annotation.Timed;
-
+import dev.langchain4j.model.chat.response.ChatResponse;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.websocket.CloseReason;
@@ -24,6 +25,11 @@ import jakarta.websocket.OnOpen;
 import jakarta.websocket.RemoteEndpoint;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import static dev.langchain4j.model.output.FinishReason.LENGTH;
+
+import io.openliberty.sample.langchain4j.StreamingChatAgent.StreamingAssistant;
 
 @ApplicationScoped
 @ServerEndpoint("/streamingchat")
@@ -32,55 +38,56 @@ public class StreamingChatService {
     private static Logger logger = Logger.getLogger(StreamingChatService.class.getName());
 
     @Inject
-    StreamingChatAgent agent = null;
+    StreamingChatAgent agent;
+
+    private StreamingAssistant assistant;
 
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session) throws Exception {
         logger.info("Server connected to session: " + session.getId());
+        assistant = agent.getStreamingAssistant();
     }
 
     @OnMessage
     @Timed(name = "chatProcessingTime", absolute = true,
            description = "Time needed chatting to the agent.")
     public void onMessage(String message, Session session) throws Exception {
-
         logger.info("Server received message \"" + message + "\" "
                     + "from session: " + session.getId());
 
-        RemoteEndpoint.Basic remote = session.getBasicRemote();
+        CompletableFuture<ChatResponse> response = new CompletableFuture<>();
+        assistant.streamingChat(session.getId(), message)
+            .onPartialResponse(token -> {
+                if (token.isEmpty())
+                    return;
+                try {
+                    session.getBasicRemote().sendText(token);
+                    Thread.sleep(100);
+                } catch (Exception error) {
+                    throw new RuntimeException(error);
+                }
+            })
+            .onCompleteResponse(response::complete)
+            .onError(response::completeExceptionally)
+            .start();
+        if (response.get().finishReason() == LENGTH)
+            session.getBasicRemote().sendText(" ...");
+	session.getBasicRemote().sendText("");
 
-        try {
-            String sessionId = session.getId();
-            switch (agent.streamingChat(sessionId, message, token -> {
-                if (token.equals("")) return;
-                remote.sendText(token);
-                Thread.sleep(100);
-            })) {
-            case STOP:
-                break;
-            default:
-                remote.sendText(" ...");
-            }
-        } catch (Exception e) {
-            remote.sendText("My failure reason is:\n\n" + e.getMessage());
-        }
-
-        remote.sendText("");
         logger.info("Server finished response to session: " + session.getId());
-
     }
 
     @OnClose
-    public void onClose(Session session, CloseReason closeReason) {
-        agent.clearChatMemory(session.getId());
-        logger.info("Session " + session.getId()
-                    + " was closed with reason " + closeReason.getCloseCode());
+    public void onClose(Session session, CloseReason closeReason) throws Exception {
+        logger.info("Session " + session.getId() + " was closed with reason " + closeReason.getCloseCode());
+	assistant.evictChatMemory(session.getId());
     }
 
     @OnError
-    public void onError(Session session, Throwable throwable) {
-        logger.info("WebSocket error for " + session.getId() + " "
-                    + throwable.getMessage());
+    public void onError(Session session, Throwable error) throws Exception {
+        logger.info("WebSocket error for " + session.getId() + " " + error.getMessage());
+	session.getBasicRemote().sendText("My failure reason is:\n\n" + error.getMessage());
+	session.getBasicRemote().sendText("");
     }
 
 }
