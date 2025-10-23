677c37ec2244daf5af412579f42a0559d31b66f6
Gilbert Kwan <gkwan@ca.ibm.com>
2025-10-23 13:29:05 -0400

    update tests (#207)
    * print out more info when having exception
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * improve the testApp.sh
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    ---------
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -12,7 +12,28 @@ fi
     -q clean package liberty:create liberty:install-feature liberty:deploy
 
 ./mvnw -ntp liberty:start
-sleep 20
+
+if [[ "$1" == "rag-db" ]]; then
+    sleep 5
+    time_out=0
+    while :
+    do
+        if [ "$(curl -s --user bob:bobpwd http://localhost:9081/api/embedding)" = "[]" ]; then
+            echo Ready to test
+            break
+        fi
+        time_out=$((time_out + 1))
+        echo waiting $time_out ...
+        sleep 5
+        if [ "$time_out" = "24" ]; then
+            echo Unable to run test
+            exit 1
+        fi
+    done
+else
+    sleep 10
+fi
+
 ./mvnw -ntp failsafe:integration-test liberty:stop
 ./mvnw -ntp failsafe:verify
 
